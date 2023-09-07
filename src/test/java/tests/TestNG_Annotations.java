b6fa400354230ace406469a5da8609e5f14eaa04
sayed <sayed.sadatibrahim@gmail.com>
2023-09-06 19:12:46 -0700

    annoations

--- src/test/java/tests/TestNG_Annotations.java
+++ src/test/java/tests/TestNG_Annotations.java
@@ -1,5 +1,85 @@
 package tests;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
 public class TestNG_Annotations {
 
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("it is before suite ");
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+
+		System.out.println("it is before test");
+
+	}
+
+	@BeforeGroups
+	public void beforeGroups() {
+
+		System.out.println("it is before groups method");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+
+		System.out.println("it is before method ");
+	}
+
+	@Test
+	public void login() {
+		System.out.println("it is login method");
+
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("it is test 2");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("it is after method");
+	}
+	
+	@AfterGroups
+	public void afterGroups() {
+		System.out.println("it is after groups method");
+	}
+	@AfterClass
+	public void afterClass() {
+		System.out.println("it is after class");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+
+		System.out.println("it is after method ");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+
+		System.out.println("it is after suite method");
+
+	}
 }
